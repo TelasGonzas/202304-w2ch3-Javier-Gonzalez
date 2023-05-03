58da38fa46453f7b77633bfc2081e8bb29746907
Javier González <jgestarriol@gmail.com>
2023-05-03 23:04:15 +0200

    refactor counter

--- counter.js
+++ counter.js
@@ -1,29 +1,38 @@
 export const counter = (displayNumber) => {
-  document.querySelector(".decrement-button").onclick = () => {
-    result = Number(displayNumber.textContent) - 1;
+  const decrementButton = document.querySelector(".decrement-button");
+  const incrementButton = document.querySelector(".increment-button");
+  const restartButton = document.querySelector(".restart-button");
+  let result = 0;
+
+  decrementButton.addEventListener("click", () => {
+    result = displayNumber.textContent - 1;
     if (result < 0) {
       result = 0;
     }
-    displayNumber.textContent = result;
-  };
 
-  document.querySelector(".increment-button").onclick = () => {
+    displayNumber.textContent = result;
+  });
+
+  incrementButton.addEventListener("click", () => {
     result = Number(displayNumber.textContent) + 1;
     if (result > 25) {
       return;
     }
-    displayNumber.textContent = result;
-  };
 
-  document.querySelector(".restart-button").onclick = () => {
+    displayNumber.textContent = result;
+  });
+
+  restartButton.addEventListener("click", () => {
     result = 0;
     displayNumber.textContent = result;
-  };
+  });
 };
 
 const startCounter = () => {
-  let displayNumber = document.querySelector(".number");
-  let result = "";
+  const displayNumber = document.querySelector(".number");
   counter(displayNumber);
 };
+
 startCounter();
+
+export default counter;
